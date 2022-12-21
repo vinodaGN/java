8a164fb1310bfe0a1fe81c0b62f979fa00c481d7
vinodaGN <117193507+vinodaGN@users.noreply.github.com>
2022-12-21 08:29:54 +0530

    Update LineEquation.java

--- LineEquation.java
+++ LineEquation.java
@@ -25,7 +25,7 @@
             // FOR Character V
             System.out.print(" ");
             for(int j=0; j<=n-1; ++j){
-               if(i==j && j<=n/2 || i+j==n-1 && j>=n/2){
+               if((i-j==n/2 && i>=n/2 || i+j==n+n/2-1)){
                System.out.print("*");
             }
             else{
